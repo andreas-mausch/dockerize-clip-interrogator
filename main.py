25094c88ac031892875b2ebc000aa3b4973fbf19
neonew <neonew@gmail.com>
2023-10-28 02:52:37 +0200

    Prepare saving tags as metadata using pyexiv2

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from PIL import Image
 from clip_interrogator import Config, Interrogator
 from glob import glob
+import pyexiv2
 import requests
 import sys
 
 ci = Interrogator(Config(clip_model_name="ViT-L-14/openai", quiet=True))
-
-print(sys.argv)
 
 for argument in sys.argv[1:]:
   for filename in glob(argument, recursive=True):
     image = Image.open(filename, mode='r')
     tags = ci.interrogate_fast(image)
     print("%s: %s" % (filename, tags))
+
+'''
+WIP: Save tags as metadata on the image
+
+    with open(filename, 'rb') as file:
+      with pyexiv2.ImageData(file.read()) as image:
+        key = 'Xmp.xmp.ClipInterrogatorDescription'
+
+        image.modify_xmp({ key: tags })
+
+        with open('./images/result.jpg', 'xb') as result:
+          result.write(image.get_bytes())
+'''
