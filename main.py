f855f0ef92688bf9d659328510d7f5661da09052
neonew <neonew@gmail.com>
2023-10-30 01:37:40 +0100

    Extract function modify_metadata()

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import pyexiv2
 import requests
 import sys
 
+def modify_metadata(path, description, metadata_key):
+  with path.open(mode='rb') as file:
+    with pyexiv2.ImageData(file.read()) as image:
+      image.modify_xmp({ metadata_key: description })
+      return image.get_bytes()
+
 @click.command()
 @click.option('--save-to-file',
               type=click.Choice(['none', 'new'], case_sensitive=False),
@@ -23,16 +29,13 @@ def clip(files, save_to_file, metadata_key):
   for argument in files:
     for path in Path.cwd().glob(argument):
       image = Image.open(path)
-      tags = ci.interrogate_fast(image)
-      print("%s: %s" % (path, tags))
+      description = ci.interrogate_fast(image)
+      print("%s: %s" % (path, description))
 
       if save_to_file == 'new':
-        with path.open(mode='rb') as file:
-          with pyexiv2.ImageData(file.read()) as image:
-            image.modify_xmp({ metadata_key: tags })
-
-            with path.with_stem(path.stem + '.clip').open(mode='xb') as result:
-              result.write(image.get_bytes())
+        image_bytes = modify_metadata(path, description, metadata_key)
+        with path.with_stem(path.stem + '.clip').open(mode='xb') as result:
+          result.write(image_bytes)
 
 if __name__ == '__main__':
   clip()
